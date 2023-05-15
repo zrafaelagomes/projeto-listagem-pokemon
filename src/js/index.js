9cb51a4ac2f11cac152e03be47c153a9949a2c04
Rafaela Gomes <131178966+zzzrafao@users.noreply.github.com>
2023-05-15 02:56:14 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagemBotaoTrocarDeTema = document.querySelector(".imagem-botao");
+
+botaoAlterarTema.addEventListener("click", () => {
+
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+    
+    body.classList.toggle("modo-escuro");
+
+    if (modoEscuroEstaAtivo) { 
+        imagemBotaoTrocarDeTema.setAttribute("src", "./src/imagens/sun.png");
+
+    } else {
+        imagemBotaoTrocarDeTema.setAttribute("src", "./src/imagens/moon.png");
+    }
+});
